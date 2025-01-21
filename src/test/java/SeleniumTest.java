a69a6082fccf503074250c92ba1f89a45574b166
sultanbee786 <sulthanbeeshaik31@gmail.com>
2025-01-21 20:31:37 +0530

    File Commit From RevPro

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -18,7 +18,7 @@ public class SeleniumTest {
     public void setUp() {
        
       // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");//linux_64
 
         // Get file
         File file = new File("src/main/java/com/revature/index.html");
